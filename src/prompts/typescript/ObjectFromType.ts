d447c63a61f14c82fbb415f589d7b9f84bf2fa97
Michael Irvine <michael.j.irvine@gmail.com>
2023-02-05 11:31:24 -0500

    adds tests for ObjectFromType

--- src/prompts/typescript/ObjectFromType.ts
+++ src/prompts/typescript/ObjectFromType.ts
@@ -14,24 +14,26 @@ class ObjectFromTypePrompt<T extends Record<string, unknown>>
   implements Prompt<T>
 {
   async getPrompt(params: Params<T>): Promise<string> {
-    return `${Object.keys(params.possibilities).map(
-      (key) =>
-        `Available ${key}: ${JSON.stringify(
-          params.possibilities[key as keyof T]
-        )}`
-    )}
-    
-    ${params.types}
-    
-    Translate the following query into a ${
+    return `${Object.keys(params.possibilities)
+      .map(
+        (key) =>
+          `Available ${key}: ${JSON.stringify(
+            params.possibilities[key as keyof T]
+          )}`
+      )
+      .join("\n")}
+
+${params.types}
+
+Translate the following query into a ${
       params.typeName
     } type represented as a JSON string:
-    Query: "${params.query}"
-    
-    Note: the current date is ${new Date().toISOString().split("T")[0]}
-    ${params.notes?.map((note) => `Note: ${note}`).join("\n")}
-    ${params.context ? `Context: ${params.context}` : ""}
-    ${params.typeName}: \``
+Query: "${params.query}"
+
+Note: the current date is ${new Date().toISOString().split("T")[0]}
+${params.notes?.map((note) => `Note: ${note}`).join("\n")}
+${params.context ? `Context: ${params.context}` : ""}
+${params.typeName}: \``
   }
 
   async run(llm: LLM, params: Params<T>): Promise<T> {
